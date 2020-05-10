1c3e5de1ac5005f6d6aa49b1c502b5b49329823e
FaizanKhan7 <mohamedfaizankhan1@gmail.com>
2020-05-10 17:21:26 +0530

    added home-carousel

--- script.js
+++ script.js
@@ -4,3 +4,49 @@ function onClickMenu() {
 
     document.getElementById("menu-bg").classList.toggle("change-bg");
 }
+
+
+const slides = document.querySelectorAll('.slide');
+const auto = true;
+const intervelTime = 5000;
+let slideInterval;
+
+const nextSlide =
+    () => {
+        // Get current class
+        const current = document.querySelector('.current');
+        // Remove current class
+        current.classList.remove('current');
+        // Check for next slide
+        if (current.nextElementSibling) {
+            // Add current to next sibling
+            current.nextElementSibling.classList.add('current');
+        } else {
+            // Add current to start
+            slides[0].classList.add('current');
+        }
+        setTimeout(() => current.classList.remove("current"));
+    }
+
+const prevSlide =
+    () => {
+        // Get current class
+        const current = document.querySelector('.current');
+        // Remove current class
+        current.classList.remove('current');
+        // Check for prev slide
+        if (current.previousElementSibling) {
+            // Add current to prev sibling
+            current.previousElementSibling.classList.add('current');
+        } else {
+            // Add current to last
+            slides[slides.length - 1].classList.add('current');
+        }
+        setTimeout(() => current.classList.remove("current"));
+    }
+
+// Auto slide
+
+if (auto) {
+    slideInterval = setInterval(nextSlide, intervelTime)
+}
