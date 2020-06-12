1ea087354a34ff458e2382ef873519751157a95c
akif00 <akifmohammed0015@gmail.com>
2020-06-12 13:59:04 +0530

    modal added

--- our-people.js
+++ our-people.js
@@ -4,3 +4,14 @@ function onClickMenu() {
 
     document.getElementById("menu-bg").classList.toggle("change-bg");
 }
+
+// Modals
+const modalLink = document.querySelector('.ripple-one');
+const modalBg = document.querySelector('.modal-bg');
+const modalClose = document.querySelector('.modal-close');
+modalLink.addEventListener('click', function () {
+    modalBg.classList.add('modal-active');
+});
+modalClose.addEventListener('click', function () {
+    modalBg.classList.remove('modal-active');
+})
